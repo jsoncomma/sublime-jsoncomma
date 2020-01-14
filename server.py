b34b2a013df7e81ec62fe4102a21963bccb7f22c
Mathieu PATUREL <australie.p@gmail.com>
2020-01-14 16:24:55 +1100

    server: can now automatically update binary

--- server.py
+++ server.py
@@ -13,8 +13,6 @@ SETTINGS = "JSONComma.sublime-settings"
 SETTINGS_EXECUTABLE = "executable_path"
 SETTINGS_AUTO_UPDATE = "automatically_update_executable"
 
-LAST_RELEASE_STORE = "jsoncomma_binary_last_release"
-
 
 class server:
     process = None
@@ -33,58 +31,59 @@ class server:
 
     @classmethod
     def start(cls):
+        """ Starts the server, downloading updates if needed
+        """
+
         running, msg = cls.is_ready()
         assert running is False, "msg: {}, process: {}, infos: {}".format(
             msg, cls.process, cls.infos
         )
 
+        # this is some shit logic, but it needs to cover all those paths
+        # auto updates on, executable doesn't exists (require update)
+        # auto updates on, executable exists (try to update)
+        # auto updates off, exectuable doesn't exists (ask to enable auto updates)
+        # auto updates off, exectuable exists (start process)
+        # TODO: add in yes/no from user
+
         settings = sublime.load_settings(SETTINGS)
-        executable_path = settings.get(SETTINGS_EXECUTABLE)
-        executable_path = os.path.expandvars(os.path.expanduser(executable_path))
+
+        if settings.get(SETTINGS_AUTO_UPDATE):
+            executable_path = cls.get_default_executable_path(expand_vars=True)
+        else:
+            executable_path = settings.get(SETTINGS_EXECUTABLE)
+            executable_path = os.path.expanduser(os.path.expandvars(executable_path))
 
         if not os.path.exists(executable_path):
-            if confirm_automatic_download(executable_path):
-                try:
-                    executable_path = cls.update_binary()
-                except Exception as e:
-                    notify("failed to download binary: {}", e)
-                    raise e
-                else:
-                    notify("done downloading jsoncomma to {}".format(executable_path))
-            else:
+            if not confirm_automatic_download(executable_path):
                 notify(
-                    "executable_path {} doesn't exist, server can't be started.",
-                    executable_path,
+                    "no binary found, and automatic updates disabled. JSONComma will not work"
                 )
-                # no executable to be found, and the user doesn't want auto installs
+                settings.set(SETTINGS_AUTO_UPDATE, False)
+                sublime.save_settings(SETTINGS)
                 return
 
-        elif settings.get(SETTINGS_AUTO_UPDATE):
-            # check for update
-            # FIXME: this shouldn't be done at *every* startup.
-            new_path = cls.update_binary()
-            # new path is none if no update was required
-            if new_path is not None:
-                executable_path = new_path
+            settings.set(SETTINGS_AUTO_UPDATE, True)
+            sublime.save_settings(SETTINGS)
+            executable_path = cls.get_default_executable_path(expand_vars=True)
 
-        cls._start(executable_path)
+        if settings.get(SETTINGS_AUTO_UPDATE):
+            try:
+                cls.auto_update_executable()
+            except requests.ConnectionError as e:
+                print("JSONComma:", e)
+                if not os.path.exists(executable_path):
+                    notify(
+                        "failed to download server due to network error, JSONComma won't work"
+                    )
+                    return
+                else:
+                    notify(
+                        "failed to automatically download server due to network error, running current version"
+                    )
 
-    @classmethod
-    def _start(cls, executable_path: str):
-        """ Just starts the actual process
-        """
-
-        # hide the terminal window on Windows
-        startupinfo = None
-        if os.name == "nt":
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-        cls.process = subprocess.Popen(
+        cls.process = start_process(
             [executable_path, "server", "-host", "localhost", "-port", "0"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            startupinfo=startupinfo,
         )
 
         line = cls.process.stdout.readline().decode("utf-8")
@@ -96,7 +95,7 @@ class server:
                     e
                 )
             )
-            print("output from the server: {!r}".format(line))
+            print("JSONComma: output from the server: {!r}".format(line))
             raise e
 
         assert "addr" in cls.infos, "server infos should include 'addr' ({})".format(
@@ -109,7 +108,7 @@ class server:
             cls.infos
         )
 
-        notify("server started on {}", cls.infos["addr"])
+        notify("server {} started on {}", executable_path, cls.infos["addr"])
 
     @classmethod
     def stop(cls):
@@ -127,16 +126,11 @@ class server:
             cls.infos = None
             return
 
-        cls.process.terminate()
-        try:
-            cls.process.wait(timeout=1)
-        except subprocess.TimeoutExpired:
-            notify("had to kill (SIGKILL) process after 1 second timeout")
-            cls.process.kill()
+        kill_nicely(cls.process)
 
         cls.process = None
         cls.infos = None
-        sublime.status_message("JSONComma: server stopped")
+        notify("JSONComma: server stopped")
 
     @classmethod
     def fix(cls, json_to_fix):
@@ -171,56 +165,40 @@ class server:
 
         return resp.text
 
+    #
     # downloading/updating the binary
+    #
 
     @classmethod
-    def get_last_release(cls) -> str:
-        """ Gets the latest tag name.
-        """
-        notify("checking last release...")
-        try:
-            resp = requests.get(
-                "https://api.github.com/repos/jsoncomma/jsoncomma/releases"
-            )
-        except requests.ConnectionError as e:
-            notify("couldn't check last version: {}", e)
-            raise e
-
-        assert (
-            resp.status_code == 200
-        ), "[getting last release] expected 200 status code, got {}".format(
-            resp.status_code
-        )
-
-        releases = resp.json()
-        for release in releases:
-            # ignore prereleases and drafts
-            if release["draft"] or release["prerelease"]:
-                continue
-            return release["tag_name"]
-
-        assert False, "no non-draft or non-prerelease release found"
-
-    @classmethod
-    def update_binary(cls):
+    def auto_update_executable(cls):
         """ install the binary in the right location, and store the path in settings.
         It returns the location of the binary. This function blocks. """
 
-        latest_tag_name = cls.get_last_release()
+        assert (
+            cls.downloading is False
+        ), "auto_update_executable called already (cls.downloading: {})".format(
+            cls.downloading
+        )
+
+        executable_path = cls.get_default_executable_path(expand_vars=True)
+
+        latest_version = cls.get_latest_version()
+
         try:
-            with open(
-                os.path.join(sublime.packages_path(), "User", LAST_RELEASE_STORE)
-            ) as fp:
-                installed_tag_name = fp.read()
-        except FileNotFoundError:
-            # do download the server, because there is currently nothing installed
-            pass
+            current_version = cls.get_current_executable_version(executable_path)
+        except FileNotFoundError as e:
+            # we are going to download it
+            notify("current version: non existent, latest version: {}", latest_version)
         else:
-            # compare the tag name, and if it's different (assume it's older), download the update
-            if installed_tag_name == latest_tag_name:
-                notify("latest release installed already")
-                return
-        notify("downloading latest version of jsoncomma {}", latest_tag_name)
+            if current_version == latest_version:
+                # don't need to update
+                return executable_path
+
+            notify(
+                "current version: {}, latest version: {}".format(
+                    current_version, latest_version
+                )
+            )
 
         cls.downloading = True
 
@@ -237,17 +215,10 @@ class server:
             "x64": "x86_64",
         }
 
-        paths = {
-            # sublime.platform() -> path for executable
-            "windows": "%APPDATA%\\jsoncomma\\jsoncomma.exe",
-            "linux": "~/.config/jsoncomma/jsoncomma",
-            "osx": "~/Library/Application Support/jsoncomma/jsoncomma",
-        }
-
         download_url = (
             "https://github.com/jsoncomma/jsoncomma/releases/download/"
-            "{tag_name}/jsoncomma_{tag_name}_{platform}_{arch}.tar.gz".format(
-                tag_name=latest_tag_name,
+            "v{version}/jsoncomma_v{version}_{platform}_{arch}.tar.gz".format(
+                version=latest_version,
                 platform=platforms[sublime.platform()],
                 arch=archs[sublime.arch()],
             )
@@ -262,10 +233,6 @@ class server:
         assert (
             resp.status_code == 200
         ), "[downloading] expected 200 status code, got {}".format(resp.status_code)
-
-        executable_path = os.path.expanduser(
-            os.path.expandvars(paths[sublime.platform()])
-        )
 
         os.makedirs(os.path.dirname(executable_path), exist_ok=True)
 
@@ -291,34 +258,82 @@ class server:
 
         os.chmod(executable_path, os.stat(executable_path).st_mode | stat.S_IEXEC)
 
-        settings = sublime.load_settings(SETTINGS)
-        # don't save executable_path because the variables in there
-        # are expanded (eg. there is no ~, but C:\Users\<username>). It's bad
-        # because it breaks portability (if the user shared his config between
-        # two machines, having the variables still in the paths is much better
-        # because the config might still work even if the values of the variables
-        # are different)
-        settings.set(SETTINGS_EXECUTABLE, paths[sublime.platform()])
-        sublime.save_settings(SETTINGS)
-
-        with open(
-            os.path.join(sublime.packages_path(), "User", LAST_RELEASE_STORE), "w"
-        ) as fp:
-            fp.write(latest_tag_name)
-
-        notify("done, jsoncomma binary at {!r}", executable_path)
+        cls.downloading = False
 
         # FIXME: maybe we should have a platform dependent settings file...
-        return executable_path
+
+    @classmethod
+    def get_latest_version(cls) -> str:
+        """ Gets the latest version from GitHub
+        """
+        notify("checking last release...")
+        resp = requests.get("https://api.github.com/repos/jsoncomma/jsoncomma/releases")
+
+        assert (
+            resp.status_code == 200
+        ), "[getting last release] expected 200 status code, got {}".format(
+            resp.status_code
+        )
+
+        releases = resp.json()
+        for release in releases:
+            # ignore prereleases and drafts
+            if release["draft"] or release["prerelease"]:
+                continue
+
+            assert release["tag_name"].startswith(
+                "v"
+            ), "expected tag name to start with 'v' in {!r}".format(release["tag_name"])
+
+            return release["tag_name"][1:]
+
+        assert False, "no non-draft or non-prerelease release found"
+
+    @classmethod
+    def get_current_executable_version(cls, executable_path):
+        process = start_process([executable_path, "-version"])
+        try:
+            exit_code = process.wait(timeout=1)
+        except subprocess.TimeoutExpired:
+            kill_nicely(process)
+            return
+
+        if exit_code != 0:
+            notify("{} -version exited with code {}", executable_path, exit_code)
+            return None
+
+        return process.stdout.readline().decode("utf-8").split(" ")[0]
+
+    @classmethod
+    def get_default_executable_path(cls, *, expand_vars):
+        assert isinstance(
+            expand_vars, bool
+        ), "expand_vars should be a boolean, got {}".format(type(expand_vars))
+
+        path = {
+            # sublime.platform() -> path for executable
+            "windows": "%APPDATA%\\jsoncomma\\jsoncomma.exe",
+            "linux": "~/.config/jsoncomma/jsoncomma",
+            "osx": "~/Library/Application Support/jsoncomma/jsoncomma",
+        }[sublime.platform()]
+
+        if expand_vars:
+            return os.path.expandvars(os.path.expanduser(path))
+        return path
 
 
 def confirm_automatic_download(current_path):
     # I'm not sure how I can make it clear that this is a one time thing.
     # if the user wants to update the server, he will have to do so manually
     return sublime.ok_cancel_dialog(
-        "The jsoncomma server was not found at '{}'. "
-        "However, it needs to be installed for JSONComma to work. "
-        "More details can be found at https://jsoncomma.github.io"
+        "The jsoncomma server was not found at '{}' "
+        "but, it needs to be installed for JSONComma to work. "
+        "More details can be found at https://jsoncomma.github.io. "
+        "If you decline, automatic updates will be disabled (you can change "
+        "that in JSONComma's settings)."
+        "\n\n"
+        "Automatic download is recommended for all users who are not "
+        "currently contributing to the development to jsoncomma."
         "\n\n"
         "Do you want JSONComma to install it for you?".format(current_path),
         "Download jsoncomma for me",
@@ -329,3 +344,33 @@ def notify(format, *args, **kwargs):
     message = "JSONComma: " + format.format(*args, **kwargs)
     print(message)
     sublime.status_message(message)
+
+
+def start_process(cmd, *args, **kwargs):
+    # hide the terminal window on Windows
+    startupinfo = None
+    if os.name == "nt":
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    # catch stdout and stderr by default
+    if "stdout" not in kwargs:
+        kwargs["stdout"] = subprocess.PIPE
+
+    if "stderr" not in kwargs:
+        kwargs["stderr"] = subprocess.STDOUT
+
+    return subprocess.Popen(cmd, startupinfo=startupinfo, *args, **kwargs)
+
+
+def kill_nicely(process, *, timeout=1):
+    """ tries to terminate, and then kills after timeout seconds
+    returns the exit code
+    """
+
+    process.terminate()
+    try:
+        return process.wait(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        process.kill()
+    return process.poll()
